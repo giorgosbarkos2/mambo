e707b17ebf93aef7334b293a39aeaecc85b0008c
vmonsalve <vicente.monsalve@gmail.com>
2014-02-04 12:41:13 -0300

    Caluga beneficio

---
+++ src/avisapp/adminBundle/Entity/calugaBeneficio.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace avisapp\adminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * calugaBeneficio
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class calugaBeneficio
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titulo", type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="text")
+     */
+    private $descripcion;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     * @return calugaBeneficio
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string 
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     * @return calugaBeneficio
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string 
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+}
